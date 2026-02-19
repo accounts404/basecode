4f900275eb552cc0a0305fedd3be5c373a8f2288
RedOak Cleaning Solutions <accounts@redoakcleaning.com.au>
2026-02-19 23:29:42 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -82,6 +82,7 @@ import ServicioActivo from './pages/ServicioActivo';
 import TVDashboard from './pages/TVDashboard';
 import TrabajoEntradas from './pages/TrabajoEntradas';
 import Vehiculos from './pages/Vehiculos';
+import Asistente from './pages/Asistente';
 import __Layout from './Layout.jsx';
 
 
@@ -121,6 +122,7 @@ export const PAGES = {
     "TVDashboard": TVDashboard,
     "TrabajoEntradas": TrabajoEntradas,
     "Vehiculos": Vehiculos,
+    "Asistente": Asistente,
 }
 
 export const pagesConfig = {
