526ecd7c7ab116fe84135e147d45383f01cd6982
RedOak Cleaning Solutions <accounts@redoakcleaning.com.au>
2026-03-03 21:13:19 +0000

    File changes

--- src/components/ui/sidebar.jsx
+++ src/components/ui/sidebar.jsx
@@ -486,6 +486,7 @@ const SidebarMenuButton = React.forwardRef(function SidebarMenuButtonInner({
 })
 SidebarMenuButton.displayName = "SidebarMenuButton"
 
+
 const SidebarMenuAction = React.forwardRef(({ className, asChild = false, showOnHover = false, ...props }, ref) => {
   const Comp = asChild ? Slot : "button"
 
